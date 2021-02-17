aa2ad9c6b7eefed64900e19eee669868fef2ee23
AndrewMautone <andrew.mautone@gmail.com>
2021-02-17 00:24:01 -0300

    Add: frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import Login from './screens/Login';
+import Register from './screens/Register';
+import Profile from './screens/Profile';
+import styled from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+const Styles = styled.div`
+  flex:1;
+  display:flex;
+  width:100vw;
+  height:100vh;
+ background-color:#131C25;
+
+
+  
+`;
+function App() {
+  return (
+
+    <Router>
+      <Styles>
+        <Switch>
+        <Route exact path='/'>
+            <Login />
+          </Route>
+          <Route exact path='/profile'>
+            <Profile />
+          </Route>
+          <Route exact path='/register'>
+            <Register />
+          </Route>
+         
+        </Switch>
+      </Styles>
+
+    </Router>
+  );
+}
+
+export default App;
